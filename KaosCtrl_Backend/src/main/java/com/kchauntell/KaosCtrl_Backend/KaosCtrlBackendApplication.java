590b3bbdf1729c69316e6b70cbaefdaf91ea9352
Kendra Miller <kendrachauntell@outlook.com>
2025-01-08 13:59:29 -0500

    initial commit with backend java folder

---
+++ KaosCtrl_Backend/src/main/java/com/kchauntell/KaosCtrl_Backend/KaosCtrlBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kchauntell.KaosCtrl_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaosCtrlBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaosCtrlBackendApplication.class, args);
+	}
+
+}
---
+++ KaosCtrl_Backend/src/test/java/com/kchauntell/KaosCtrl_Backend/KaosCtrlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kchauntell.KaosCtrl_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KaosCtrlBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
